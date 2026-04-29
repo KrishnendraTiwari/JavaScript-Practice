55e6735782cb9e116305cca421b08a407450575c
KrishnendraTiwari <krishnendratiwari09@gmail.com>
2026-04-29 15:34:53 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1357
+let accountEmail = "badal09@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2  not allowed
+
+console.log(accountId);
+
+accountEmail = "jitu05@gmail.com"
+accountPassword = "teribaiski"
+accountCity = "Australia"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
